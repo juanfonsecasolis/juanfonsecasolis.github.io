5fb1d8c09ff182739cd73c666330afbf594ffa34
Juan Fonseca <juanma2268@gmail.com>
2020-09-06 14:55:46 -0600

    Added notebook about pairwise testing

--- blog/ipynb2html.sh
+++ blog/ipynb2html.sh
@@ -2,18 +2,22 @@
 jupyter nbconvert JFonseca.suavizadoTraficoServidorWeb.ipynb --to html --template template.tpl
 jupyter nbconvert JFonseca.interpolacionBL.ipynb --to html --template template.tpl
 jupyter nbconvert JFonseca.pruebasRendimiento.ipynb --to html --template template.tpl
+jupyter nbconvert JFonseca.pairwisetesting.ipynb --to html --template template.tpl
 
 sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.interpolacionBL.html > JFonseca.interpolacionBL2.html
 sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.suavizadoTraficoServidorWeb.html > JFonseca.suavizadoTraficoServidorWeb2.html
 sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.pruebasRendimiento.html > JFonseca.pruebasRendimiento2.html
+sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.pairwisetesting.html > JFonseca.pairwisetesting2.html
 
 rm JFonseca.interpolacionBL.html
 rm JFonseca.suavizadoTraficoServidorWeb.html
 rm JFonseca.pruebasRendimiento.html
+rm JFonseca.pairwisetesting.html
 
 mv JFonseca.interpolacionBL2.html JFonseca.interpolacionBL.html
 mv JFonseca.suavizadoTraficoServidorWeb2.html JFonseca.suavizadoTraficoServidorWeb.html
 mv JFonseca.pruebasRendimiento2.html JFonseca.pruebasRendimiento.html
+mv JFonseca.pairwisetesting2.html JFonseca.pairwisetesting.html
 
 #awk '{gsub(/max-width: calc(100% - 14ex);/,"max-width: 100%;")}' $1.html
 
