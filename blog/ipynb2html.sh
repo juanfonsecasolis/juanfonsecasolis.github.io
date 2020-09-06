f4168b3bf646b5ac167671755ab45997e02274e2
Juan Fonseca <juanma2268@gmail.com>
2020-09-05 22:04:45 -0600

    Converted ipython to html

--- blog/ipynb2html.sh
+++ blog/ipynb2html.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-ipython nbconvert JFonseca.suavizadoTraficoServidorWeb.ipynb --to html --template template.tpl
-ipython nbconvert JFonseca.interpolacionBL.ipynb --to html --template template.tpl
+jupyter nbconvert JFonseca.suavizadoTraficoServidorWeb.ipynb --to html --template template.tpl
+jupyter nbconvert JFonseca.interpolacionBL.ipynb --to html --template template.tpl
+jupyter nbconvert JFonseca.pruebasRendimiento.ipynb --to html --template template.tpl
 
 sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.interpolacionBL.html > JFonseca.interpolacionBL2.html
 sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.suavizadoTraficoServidorWeb.html > JFonseca.suavizadoTraficoServidorWeb2.html
+sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.pruebasRendimiento.html > JFonseca.pruebasRendimiento2.html
 
 rm JFonseca.interpolacionBL.html
 rm JFonseca.suavizadoTraficoServidorWeb.html
+rm JFonseca.pruebasRendimiento.html
 
 mv JFonseca.interpolacionBL2.html JFonseca.interpolacionBL.html
 mv JFonseca.suavizadoTraficoServidorWeb2.html JFonseca.suavizadoTraficoServidorWeb.html
+mv JFonseca.pruebasRendimiento2.html JFonseca.pruebasRendimiento.html
 
 #awk '{gsub(/max-width: calc(100% - 14ex);/,"max-width: 100%;")}' $1.html
 
