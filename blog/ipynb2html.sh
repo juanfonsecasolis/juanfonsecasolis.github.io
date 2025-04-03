88ea70c610f652eeaea842579dde248b7b8e6cbf
Juan Fonseca <juan.fonsecasolis@outlook.com>
2025-04-03 11:39:19 -0600

    Worked on SOLID notebook...

--- blog/ipynb2html.sh
+++ blog/ipynb2html.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-#jupyter nbconvert JFonseca.suavizadoTraficoServidorWeb.ipynb --to html --template template.tpl
-#jupyter nbconvert JFonseca.interpolacion.ipynb --to html --template template.tpl
-#jupyter nbconvert JFonseca.pruebasRendimiento.ipynb --to html --template template.tpl
-#jupyter nbconvert JFonseca.pairwisetesting.ipynb --to html --template template.tpl
-#jupyter nbconvert JFonseca.evaluacionPerdidaCalidadAudioOpus.ipynb --to html --template template.tpl
-#jupyter nbconvert JFonseca.pruebasFormalesSoftware.ipynb --to html --template template.tpl
-#jupyter nbconvert JFonseca.audioVideoQualityStreaming.ipynb --to html --template template.tpl
-#jupyter nbconvert JFonseca.wavelets.ipynb --to html --template template.tpl
-#jupyter nbconvert JFonseca.consejosIniciarRolAutomatizadorPruebas.ipynb to html --template template.tpl
 
 jupyter nbconvert JFonseca.suavizadoTraficoServidorWeb.ipynb --to html
 jupyter nbconvert JFonseca.interpolacion.ipynb --to html
@@ -17,6 +8,7 @@ jupyter nbconvert JFonseca.evaluacionPerdidaCalidadAudioOpus.ipynb --to html
 jupyter nbconvert JFonseca.pruebasFormalesSoftware.ipynb --to html
 jupyter nbconvert JFonseca.audioVideoQualityStreaming.ipynb --to html
 jupyter nbconvert JFonseca.wavelets.ipynb --to html
+jupyter nbconvert JFonseca.solid.ipynb --to html
 jupyter nbconvert JFonseca.consejosIniciarRolAutomatizadorPruebas.ipynb --to html
 
 sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.interpolacion.html > JFonseca.interpolacion2.html
@@ -27,6 +19,7 @@ sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.evaluacionPerd
 sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.pruebasFormalesSoftware.html > JFonseca.pruebasFormalesSoftware2.html
 sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.audioVideoQualityStreaming.html > JFonseca.audioVideoQualityStreaming2.html
 sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.wavelets.html > JFonseca.wavelets2.html
+sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.solid.html > JFonseca.solid2.html
 sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.consejosIniciarRolAutomatizadorPruebas.html > JFonseca.consejosIniciarRolAutomatizadorPruebas2.html
 
 rm JFonseca.interpolacion.html
@@ -37,6 +30,7 @@ rm JFonseca.evaluacionPerdidaCalidadAudioOpus.html
 rm JFonseca.pruebasFormalesSoftware.html
 rm JFonseca.audioVideoQualityStreaming.html
 rm JFonseca.wavelets.html
+rm JFonseca.solid.html
 rm JFonseca.consejosIniciarRolAutomatizadorPruebas.html
 
 mv JFonseca.interpolacion2.html JFonseca.interpolacion.html
@@ -47,4 +41,5 @@ mv JFonseca.evaluacionPerdidaCalidadAudioOpus2.html JFonseca.evaluacionPerdidaCa
 mv JFonseca.pruebasFormalesSoftware2.html JFonseca.pruebasFormalesSoftware.html
 mv JFonseca.audioVideoQualityStreaming2.html JFonseca.audioVideoQualityStreaming.html
 mv JFonseca.wavelets2.html JFonseca.wavelets.html
+mv JFonseca.solid2.html JFonseca.solid.html
 mv JFonseca.consejosIniciarRolAutomatizadorPruebas2.html JFonseca.consejosIniciarRolAutomatizadorPruebas.html
