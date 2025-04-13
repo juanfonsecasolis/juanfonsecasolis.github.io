8fbb6b02715419fbad0beb4387591c9ff7da25fe
Juan Fonseca <juan.fonsecasolis@outlook.com>
2025-04-13 16:17:57 -0600

    Finished SOLID notebook.

--- blog/ipynb2html.sh
+++ blog/ipynb2html.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-jupyter nbconvert JFonseca.suavizadoTraficoServidorWeb.ipynb --to html
-jupyter nbconvert JFonseca.interpolacion.ipynb --to html
-jupyter nbconvert JFonseca.pruebasRendimiento.ipynb --to html
-jupyter nbconvert JFonseca.pairwisetesting.ipynb --to html
-jupyter nbconvert JFonseca.evaluacionPerdidaCalidadAudioOpus.ipynb --to html
-jupyter nbconvert JFonseca.pruebasFormalesSoftware.ipynb --to html
-jupyter nbconvert JFonseca.audioVideoQualityStreaming.ipynb --to html
-jupyter nbconvert JFonseca.wavelets.ipynb --to html
-jupyter nbconvert JFonseca.solid.ipynb --to html
-jupyter nbconvert JFonseca.consejosIniciarRolAutomatizadorPruebas.ipynb --to html
+python3 -m nbconvert JFonseca.suavizadoTraficoServidorWeb.ipynb --to html
+python3 -m nbconvert JFonseca.interpolacion.ipynb --to html
+python3 -m nbconvert JFonseca.pruebasRendimiento.ipynb --to html
+python3 -m nbconvert JFonseca.pairwisetesting.ipynb --to html
+python3 -m nbconvert JFonseca.evaluacionPerdidaCalidadAudioOpus.ipynb --to html
+python3 -m nbconvert JFonseca.pruebasFormalesSoftware.ipynb --to html
+python3 -m nbconvert JFonseca.audioVideoQualityStreaming.ipynb --to html
+python3 -m nbconvert JFonseca.wavelets.ipynb --to html
+python3 -m nbconvert JFonseca.solid.ipynb --to html
+python3 -m nbconvert JFonseca.consejosIniciarRolAutomatizadorPruebas.ipynb --to html
+python3 -m nbconvert JFonseca.solid.ipynb --to html
 
 sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.interpolacion.html > JFonseca.interpolacion2.html
 sed 's/max-width: calc(100% - 14ex);/max-width: 100%;/g' JFonseca.suavizadoTraficoServidorWeb.html > JFonseca.suavizadoTraficoServidorWeb2.html
