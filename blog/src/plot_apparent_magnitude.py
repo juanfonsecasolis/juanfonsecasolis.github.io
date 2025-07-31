008cc9c058f7507d373e40473f512085e1a38159
Juan Fonseca <juan.fonsecasolis@outlook.com>
2025-07-31 17:42:11 -0600

    Code refactoring for Our Lady of Guadalupe.

--- blog/src/plot_apparent_magnitude.py
+++ blog/src/plot_apparent_magnitude.py
@@ -1,41 +1,34 @@
+#
 # 2025 Juan M. Fonseca-Solis
-# ! python -m pip install lightkurve oktopus autograd
+#
+# Referencias: 
 # https://lightkurve.github.io/lightkurve/quickstart.html
 # https://docs.astropy.org/en/stable/index_getting_started.html
+# https://github.com/lightkurve/lightkurve/discussions/1337#discussioncomment-5866342
 
 import lightkurve as lk
 import matplotlib.pyplot as plt
 import numpy as np
-from datetime import datetime
-
-# Using Flamsteed format.
-corona_borealis_constellation = [
-    '3 CrB', # beta
-    '5 CrB', # alpha
-    '4 CrB', # theta
-    '8 CrB', # gamma
-    '10 CrB', # delta 
-    '13 CrB', # epsilon (Nusakan)
-    '14 CrB' # iota
-    ]
+from corona_borealis import flamsteed_CrB
 
 fig, ax = plt.subplots()
-for star in corona_borealis_constellation:
+for starName in flamsteed_CrB:
 
-    print(f'Downloading data for {star}...')
+    print(f'Downloading data for {starName}...')
+
     try:
-        lc = lk.search_lightcurve(star, radius=0)[0].download()
-        # https://github.com/lightkurve/lightkurve/discussions/1337#discussioncomment-5866342
+        lc = lk.search_lightcurve(flamsteed_CrB[starName], radius=0)[0].download()
         apparent_magnitude = lc.meta['TESSMAG'] + -2.5*np.log10(lc.flux.value/np.nanmedian(lc.flux.value))
         print('Plotting...')
         days_of_the_year = [date.month*30+date.day for date in lc.time.to_value('datetime')]
         plt.plot(days_of_the_year, apparent_magnitude)
-    except Exception as e:
-        print(f'{repr(e)} error while downloading data for {star}. Skipping...')
 
-plt.legend(corona_borealis_constellation)
+    except Exception as e:
+        print(f'{repr(e)} error while downloading data for {starName}. Skipping...')
+
+plt.legend(flamsteed_CrB.keys())
 plt.ylabel('Apparent magnitude')
 plt.xlabel('Day of the year')
-#ax.tick_params(axis='x', labelrotation=45)
+plt.title('Corona Borealis')
 plt.show()
-print('Done')
+print('Done.')
--- blog/src/plot_coordinates.py
+++ blog/src/plot_coordinates.py
@@ -1,36 +1,29 @@
+#
 # 2025 Juan M. Fonseca-Solis
-# ! python -m pip install lightkurve oktopus autograd
+#
+# Referencias: 
 # https://lightkurve.github.io/lightkurve/tutorials/1-getting-started/using-target-pixel-file-products.html?highlight=skycoord
 # https://en.wikipedia.org/wiki/List_of_stars_in_Corona_Borealis
 
 import lightkurve as lk
 import matplotlib.pyplot as plt
-
-# Using Flamsteed format.
-corona_borealis_constellation = [
-    '3 CrB', # beta
-    '5 CrB', # alpha
-    '4 CrB', # theta
-    '8 CrB', # gamma
-    '10 CrB', # delta 
-    '13 CrB', # epsilon (Nusakan)
-    '14 CrB' # iota
-    ]
+from corona_borealis import flamsteed_CrB
 
 fig, ax = plt.subplots()
-for star in corona_borealis_constellation:
+for starName in flamsteed_CrB:
 
-    print(f'Downloading data for {star}...')
+    print(f'Downloading data for {starName}...')
     try:
-        targetPixelFile = lk.search_targetpixelfile(star, radius=0)[0].download()
+        targetPixelFile = lk.search_targetpixelfile(flamsteed_CrB[starName], radius=0)[0].download()
         print('Plotting...')
         cartesian_coordinates = targetPixelFile.wcs.pixel_to_world(0, 0).cartesian
         plt.scatter(cartesian_coordinates.x, cartesian_coordinates.y)
     except Exception as e:
-        print(f'"{repr(e)}" error while downloading data for {star}. Skipping...')
+        print(f'"{repr(e)}" error while downloading data for {starName}. Skipping...')
 
-plt.legend(corona_borealis_constellation)
+plt.legend(flamsteed_CrB.keys())
 plt.ylabel('Y')
 plt.xlabel('X')
+plt.title('Corona Borealis')
 plt.show()
-print('Done')
+print('Done.')
