441e5c74d290d5bb437b109d35ae021c98a882c2
Juan Fonseca <juan.fonsecasolis@outlook.com>
2025-09-03 09:50:55 -0600

    Generated SOLID notebook again.

--- blog/src/plot_apparent_magnitude.py
+++ blog/src/plot_apparent_magnitude.py
@@ -5,6 +5,8 @@
 # https://lightkurve.github.io/lightkurve/quickstart.html
 # https://docs.astropy.org/en/stable/index_getting_started.html
 # https://github.com/lightkurve/lightkurve/discussions/1337#discussioncomment-5866342
+# https://lightkurve.github.io/lightkurve/reference/api/lightkurve.search_lightcurve.html?highlight=search_lightcurve
+# https://spacetelescope.github.io/mast_notebooks/notebooks/Kepler/lightkurve_searching_for_data/lightkurve_searching_for_data.html#performing-a-cone-search
 
 import lightkurve as lk
 import matplotlib.pyplot as plt
@@ -17,11 +19,17 @@ for starName in flamsteed_CrB:
     print(f'Downloading data for {starName}...')
 
     try:
-        lc = lk.search_lightcurve(flamsteed_CrB[starName], radius=0)[0].download()
-        apparent_magnitude = lc.meta['TESSMAG'] + -2.5*np.log10(lc.flux.value/np.nanmedian(lc.flux.value))
-        print('Plotting...')
-        days_of_the_year = [date.month*30+date.day for date in lc.time.to_value('datetime')]
-        plt.plot(days_of_the_year, apparent_magnitude)
+        light_curves = lk.search_lightcurve(flamsteed_CrB[starName], radius=0).download_all()
+
+        for light_curve in [light_curves[0]]:
+
+            days_of_the_year = [date.month*30+date.day for date in light_curve.time.to_value('datetime')]
+            apparent_magnitude = light_curve.meta['TESSMAG'] + -2.5*np.log10(light_curve.flux.value/np.nanmedian(light_curve.flux.value)) 
+            
+            print(f'Type "days_of_the_year" = "{type(days_of_the_year)}"')
+            print(f'Type "apparent_magnitude" = "{type(apparent_magnitude)}"')
+            print('Plotting...')
+            plt.plot(days_of_the_year, apparent_magnitude)
 
     except Exception as e:
         print(f'Error while downloading data for {starName}. Skipping...\n')
