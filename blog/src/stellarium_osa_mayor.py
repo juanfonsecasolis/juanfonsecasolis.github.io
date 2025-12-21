70de91c75a463865f0993cdeca9db5e5e6090c5b
juanf <juan.fonsecasolis@outlook.com>
2025-12-21 17:02:26 -0600

    Implemented StarCoordinate class.

--- blog/src/stellarium_osa_mayor.py
+++ blog/src/stellarium_osa_mayor.py
@@ -5,21 +5,22 @@ Estrellas al momento de la impregnación de Nuestra Señora de Guadalupe
 
 import requests
 import numpy as np
+from star_coordinate import StarCoordinate
 
 ursa_major_mapping = [
-    { 'hr_classification': 'HR-5191', 'flamsteed': '85 Ursae Majoris', 'name': 'Alkaid'},
-    { 'hr_classification': 'HR-5054', 'flamsteed': '79 Ursae Majoris', 'name': 'Mizar A'},
-    { 'hr_classification': 'HR-4905', 'flamsteed': '77 Ursae Majoris', 'name': 'Alioth'},
-    { 'hr_classification': 'HR-4660', 'flamsteed': '69 Ursae Majoris', 'name': 'Megrez'},
-    { 'hr_classification': 'HR-4554', 'flamsteed': '64 Ursae Majoris', 'name': 'Phecda'},
-    { 'hr_classification': 'HR-4295', 'flamsteed': '48 Ursae Majoris', 'name': 'Merak'},
-    { 'hr_classification': 'HR-4301', 'flamsteed': '50 Ursae Majoris', 'name': 'Dubhe'},
-    { 'hr_classification': 'HR-3757', 'flamsteed': '23 Ursae Majoris', 'name': '23 UMa'},
-    { 'hr_classification': 'HR-3323', 'flamsteed': '01 Ursae Majoris', 'name': 'ο UMa'},
-    { 'hr_classification': 'HR-3888', 'flamsteed': '29 Ursae Majoris', 'name': 'υ UMa'}
+    StarCoordinate('HR-5191', '85 Ursae Majoris', 'Alkaid'),
+    StarCoordinate('HR-5054', '79 Ursae Majoris', 'Mizar A'),
+    StarCoordinate('HR-4905', '77 Ursae Majoris', 'Alioth'),
+    StarCoordinate('HR-4660', '69 Ursae Majoris', 'Megrez'),
+    StarCoordinate('HR-4554', '64 Ursae Majoris', 'Phecda'),
+    StarCoordinate('HR-4295', '48 Ursae Majoris', 'Merak'),
+    StarCoordinate('HR-4301', '50 Ursae Majoris', 'Dubhe'),
+    StarCoordinate('HR-3757', '23 Ursae Majoris', '23 UMa'),
+    StarCoordinate('HR-3323', '01 Ursae Majoris', 'ο UMa'),
+    StarCoordinate('HR-3888', '29 Ursae Majoris', 'υ UMa')
 ]
 
-ursa_pais = [
+ursa_pair = [
     ['HR-5191', 'HR-5054'],
     ['HR-5054', 'HR-4905'],
     ['HR-4905', 'HR-4660'],
@@ -54,6 +55,9 @@ def setup_location():
         raise Exception('Error setting the location')
 
 def get_star_information(star_name):
+    '''
+     To-Do: check against logic of https://www.celestialprogramming.com/decimal_degrees_to_components.html
+    '''
     response = requests.get(f'{base_url}/api/objects/info?name={star_name}&format=json')
     if response.status_code == 200:
         data = response.json()
@@ -61,32 +65,36 @@ def get_star_information(star_name):
     else:
         raise Exception(f'Could not find star with name "{star_name}".')
 
-    # To-Do: check against logic of https://www.celestialprogramming.com/decimal_degrees_to_components.html
-
 def print_ar_dec(ar, dec):
     ar_hours, ar_minutes, ar_seconds = decimalToDegrees(ar)
     dec_hours, dec_minutes, dec_seconds = decimalToDegrees(dec)
     ar_str = f'{ar} ({ar_hours}h {ar_minutes}m {ar_seconds}s)'
     dec_str = f'{dec} ({dec_hours}h {dec_minutes}m {dec_seconds}s)'
-    print(f'AR/Dec (en fecha): {ar_str} / {dec_str}\n')
+    print(f'AR/Dec (en fecha): {ar_str} / {dec_str}\n') 
+
+def fill_with_magnitude_and_ecuatorial_coordinates(stars:list[StarCoordinate]):
+
+    for i in range(0, len(stars)):
+
+        try:
+            print(f'Estrella: {stars[i].common_name}')
+            print('-----------')
+            mag, ar, dec = get_star_information(stars[i].common_name)
+            stars[i].magnitude = mag
+            stars[i].ar = ar
+            stars[i].dec = dec
+
+            print(f'Magnitud: {stars[i].magnitude:.2f}')
+            print_ar_dec(stars[i].ar, stars[i].dec)
+
+        except Exception as e:
+            print(f'{str(e)}\n')
 
 if __name__ == '__main__':
     setup_time()
     setup_location()
+    fill_with_magnitude_and_ecuatorial_coordinates(ursa_major_mapping)
 
-    for i in range(0, len(ursa_major_mapping)):
+    
 
-        try:
-            star_name = ursa_major_mapping[i]['name']
-            print(f'Estrella: {star_name}')
-            print('-----------')
-            mag, ar, dec = get_star_information(star_name)
-            ursa_major_mapping[i]['mag'] = mag
-            ursa_major_mapping[i]['ar'] = ar
-            ursa_major_mapping[i]['dec'] = dec
-
-            print(f'Magnitud: {mag:.2f}')
-            print_ar_dec(ar, dec)
-
-        except Exception as e:
-            print(f'{str(e)}\n')
+    
