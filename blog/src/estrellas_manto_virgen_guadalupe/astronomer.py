6c8b8d110539a2de596de8240d089736156494cc
Juan Fonseca <juan.fonsecasolis@outlook.com>
2026-01-25 18:45:57 -0600

    Moved code to find stars by name to the constellation method.

--- blog/src/estrellas_manto_virgen_guadalupe/astronomer.py
+++ blog/src/estrellas_manto_virgen_guadalupe/astronomer.py
@@ -11,37 +11,30 @@ from star import Star
 class Astronomer:
 
     def __init__(self):
+        self.deg_to_rad_coeff = 2.0 * np.pi / 360.0
+        self.rad_to_deg_coeff = 1.0 / self.deg_to_rad_coeff
         pass
-
-    def find_first_star_that_matches_hr_name(self, constellation:Constellation, hr_name:str):
-        results = [x for x in constellation.stars if x.hr_name==hr_name]
-        return None if results is None else results[0]
 
     def find_kilometers_between_star_pair(self, star1:Star, star2:Star) -> float:
         '''
         Kilometers distance in the celestial sphere.
         '''
-        deg_to_rad_coeff = 2.0 * np.pi / 360.0
-        rad_to_deg_coeff = 1.0 / deg_to_rad_coeff
+        
+        dec1_rad = star1.dec * self.deg_to_rad_coeff
+        ar1_rad = star1.ar * self.deg_to_rad_coeff
 
-        dec1_rad = star1.dec * deg_to_rad_coeff
-        ar1_rad = star1.ar * deg_to_rad_coeff
-
-        dec2_rad = star2.dec * deg_to_rad_coeff
-        ar2_rad = star2.ar * deg_to_rad_coeff
+        dec2_rad = star2.dec * self.deg_to_rad_coeff
+        ar2_rad = star2.ar * self.deg_to_rad_coeff
 
         d_rad = np.arccos(np.sin(dec1_rad) * np.sin(dec2_rad) + np.cos(dec1_rad) * np.cos(dec2_rad) * np.cos(ar1_rad-ar2_rad))
-        return d_rad * rad_to_deg_coeff * 111.23
+        return d_rad * self.rad_to_deg_coeff * 111.23
 
-    def find_kilometers_between_star_pairs(self, pairs:list[list[str]], constellation:Constellation) -> dict:
+    def find_kilometers_between_star_pairs(self, pairs:list[list[Star]]) -> dict:
+
         distances = {}
-
-        for pair in pairs:
-            star1 = self.find_first_star_that_matches_hr_name(constellation, pair[0])
-            star2 = self.find_first_star_that_matches_hr_name(constellation, pair[1])
+        for [star1, star2] in pairs:
             d_km = self.find_kilometers_between_star_pair(star1, star2) if star1.dec is not None and star2.dec is not None else np.nan
-            distances[(pair[0], pair[1])] = float(np.round(d_km, 2))
-
+            distances[(star1.hr_name, star2.hr_name)] = float(np.round(d_km, 2))
         return distances
 
     def computer_pearson_correlation(self, x:list[float], y:list[float]) -> float:
