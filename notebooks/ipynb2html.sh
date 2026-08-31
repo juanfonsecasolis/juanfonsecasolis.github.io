bd103fa82b75676d0b34e5c02b7743f73438a14f
juanf <juan.fonsecasolis@outlook.com>
2026-08-30 19:32:50 -0600

    Added Our Lady of Guadalupe notebook.

--- notebooks/ipynb2html.sh
+++ notebooks/ipynb2html.sh
@@ -3,7 +3,7 @@
 # 1. https://stackoverflow.com/questions/12317483/array-of-arrays-in-bash#:~:text=The%20basis%20of%20this%20answer,it%20was%20an%20n%2Dd%20array.
 
 NB_CONVERT="python3 -m nbconvert"
-FLAGS="--output-dir='../html' --to html --template=../template.tpl --config=../conf.json --embed-images"
+FLAGS="--from markdown+footnotes --output-dir='../html' --to html --template=../template.tpl --config=../conf.json --embed-images"
 
 notebooks=("principiosSolid" "transformadaOndeletas" "evaluacionCodecOpus" "pruebasDePares" "pruebasEstresSistemasLIT" "echoesAndReverberations" "movingAverageEarlyAlerts" "interpolationAsymetricKernels")
 for notebook in "${notebooks[@]}"; do
